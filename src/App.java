7312ee678d33ba60016c561f6f9bb670ad23cfb9
Holle_Wurze <goggeliz@gmail.com>
2021-10-31 12:30:28 +0200

    Initial upload

---
+++ src/App.java
@@ -0,0 +1,12 @@
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        int a = 5;
+        int b = 5;
+        int c;
+        c = a + b;
+        System.out.println(c);
+
+    }
+}
